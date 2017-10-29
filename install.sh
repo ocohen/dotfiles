7c327dc2ddac5ab35a8792f9716ccc0fbfd67a5d
ocohen <oricohen139@gmail.com>
2017-10-29 01:29:29 -0400

    Added zshrc

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/sh
 echo `echo "source ~/dotfiles/.vimrc" >> ~/.vimrc`
 echo `echo "source-file ~/dotfiles/.tmux.conf" >> ~/.tmux.conf`
+echo `echo "source ~/dotfiles/.zshrc" >> ~/.zshrc`
